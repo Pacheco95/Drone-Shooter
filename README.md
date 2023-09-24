c41f85ee28e5eda11db27d97cd1e915b0fb683ef
Michael Pacheco <mdpgd95@gmail.com>
2023-09-24 01:08:45 -0300

    feat: add screenshot to README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Drone Shooter
+
 Project made with [this](https://www.youtube.com/watch?v=nAh_Kx5Zh5Q&ab_channel=ClearCode) tutorial
+
+![](screenshot.png)
--- scenes/enemies/drone/drone.gd
+++ scenes/enemies/drone/drone.gd
@@ -3,6 +3,6 @@ extends CharacterBody2D
 func _process(_delta):
 	const direction = Vector2.RIGHT
 
-	velocity = direction * 100
+	# velocity = direction * 100
 
 	move_and_slide()
